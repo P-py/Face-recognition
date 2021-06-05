dd0bcbb9fa4720794099a2e1ab3dea89f0e132bd
Pedro Salviano Santos <pedrosalviano170@gmail.com>
2021-06-05 00:52:27 -0300

    Initial Commit

---
+++ face_recognition_img.py
@@ -0,0 +1,32 @@
+import tkinter
+import cv2
+from tkinter import filedialog
+
+# Hides the root of tkinter
+root = tkinter.Tk()
+root.withdraw()
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Ask the user for image input
+filepath = filedialog.askopenfilename()
+
+# Read the input image
+img = cv2.imread(filepath)
+
+# Convert the image into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+if faces.any() == True:
+    print("FOUND A FACE!!")
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
